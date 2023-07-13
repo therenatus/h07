096c0c9791dfa91066b1b9bdc1bbbe25cc940257
therenatus <therinatus@gmail.com>
2023-07-13 16:17:52 +0600

    add gitignore

--- dist/controller/validator/create-post-with-param.validator.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreatePostWithParamValidator = void 0;
-const express_validator_1 = require("express-validator");
-exports.CreatePostWithParamValidator = [
-    (0, express_validator_1.body)('title').trim().isString().isLength({ min: 1, max: 15 }),
-    (0, express_validator_1.body)('shortDescription').trim().isLength({ min: 1, max: 100 }),
-    (0, express_validator_1.body)('content').trim().isLength({ min: 1, max: 1000 })
-];
--- dist/types/query.interface.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var directionEnum;
-(function (directionEnum) {
-    directionEnum["ASC"] = "asc";
-    directionEnum["DESC"] = "desc";
-})(directionEnum || (directionEnum = {}));
