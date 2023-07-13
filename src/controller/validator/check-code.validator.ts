274a5509dae88757ed10cad172af9bfd280b3c83
therenatus <therinatus@gmail.com>
2023-07-13 16:59:58 +0600

    fix login service

--- src/controller/validator/check-code.validator.ts
+++ src/controller/validator/check-code.validator.ts
@@ -4,7 +4,7 @@ import { userCollection} from "../../index";
 export const CheckCodeValidator = [
   body('code').trim().isString().custom(async(code) => {
     const user = await userCollection.findOne({'emailConfirmation.confirmationCode': code});
-    if(!user) {
+    if(!user || user.emailConfirmation.confirmationCode != code) {
       throw new Error('Code not found');
     }
     return true;
